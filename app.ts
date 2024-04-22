de1c25ac8457d468aa2b4298c38b33750183f543
Collinsolayemi <collinsolayemi@gmail.com>
2024-04-22 16:27:37 +0100

    changes

--- app.ts
+++ app.ts
@@ -5,16 +5,41 @@ import authRouter from './src/route/user.route';
 import { AppDataSource } from './src/datasource/datasource';
 import errorMiddleware from './src/middleware/error/error.middleware';
 dotenv.config({ path: './.env' });
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { url } from 'inspector';
 
 const app = express();
 
 const port = process.env.PORT;
 
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Sample API',
+      version: '1.0.0',
+      description: 'A sample API with Swagger documentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ['dist/src/route/*.js'],
+};
+
+// Initialize Swagger-jsdoc
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', authRouter);
-
 
 app.use(errorMiddleware);
 
@@ -27,7 +52,7 @@ AppDataSource.initialize()
   })
   .catch((error) => console.log(error));
 
-  //to add user to the req body globally
+//to add user to the req body globally
 declare global {
   namespace Express {
     interface Request {
