c16f59e346a3c663362409604cc667502a09762a
Collinsolayemi <collinsolayemi@gmail.com>
2024-04-22 08:37:32 +0100

    updated the appsource

--- src/datasource/datasource.ts
+++ src/datasource/datasource.ts
@@ -1,21 +1,21 @@
 import 'reflect-metadata';
-import { DataSource, getMetadataArgsStorage } from 'typeorm';
+import { DataSource } from 'typeorm';
 
 import * as dotenv from 'dotenv';
 import { User } from '../entity/user.entity';
 
 dotenv.config();
 
-const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, NODE_ENV } =
-  process.env;
+const { DB_HOST, DB_USERNAME, DB_DATABASE, NODE_ENV, DB_URL } = process.env;
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
+  url: DB_URL,
+  host: DB_HOST,
   port: 3306,
-  username: 'root',
+  username: DB_USERNAME,
   password: '',
-  database: 'sterling-dictionary',
+  database: DB_DATABASE,
   entities: [User],
   migrations: [__dirname + '../migration/*.ts'],
   synchronize: true,
